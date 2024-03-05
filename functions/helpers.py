67eabeb6e5dc3ace23303f9243bdec11bd17149f
Harsh Devmurari <harshdevmurari007@gmail.com>
2024-03-05 17:37:17 +0530

    crop_recommendation_done

--- functions/helpers.py
+++ functions/helpers.py
@@ -1,6 +1,7 @@
 def crop_predict(N,P,K,temperature,humidity,ph,rainfall):
     import joblib
-    model="/home/harsh/HARSH/projects/vcet hackathon 2k22 takshak farmer assistant/functions/croprec.pkl"
+    import sklearn
+    model="/home/devmurari/HARSH/PROJECT_OPS/takshak_farmer_assistant_vcet_hackathon_2022/takshak/functions/crop_predict.pkl"
     model = joblib.load(model)
     prediction=model.predict([[N,P,K,temperature,humidity,ph,rainfall]])
     return prediction
